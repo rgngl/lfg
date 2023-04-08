4892facb38a3e8a7cbc347507919ba7d1d99ff68
Üstün Ergenoglu <ustun@noice.com>
2023-04-08 10:42:33 +0300

    Tidy up tests

--- queue_test.go
+++ queue_test.go
@@ -12,49 +12,67 @@ type testMsg struct{}
 func TestRingBufferSingleThread(t *testing.T) {
 	b := New[int](4)
 
-	v, ok := b.Dequeue()
-	assert.False(t, ok)
-	assert.Nil(t, v)
-
-	ok = b.Enqueue(intPtr(0))
-	assert.True(t, ok)
-
-	v, ok = b.Dequeue()
-	assert.True(t, ok)
-	assert.Equal(t, 0, *v)
-
-	ok = b.Enqueue(intPtr(1))
-	assert.True(t, ok)
-	ok = b.Enqueue(intPtr(2))
-	assert.True(t, ok)
-	ok = b.Enqueue(intPtr(3))
-	assert.True(t, ok)
-	ok = b.Enqueue(intPtr(4))
-	assert.False(t, ok)
-
-	v, ok = b.Dequeue()
-	assert.True(t, ok)
-	assert.Equal(t, 1, *v)
-
-	ok = b.Enqueue(intPtr(5))
-	assert.True(t, ok)
-
-	v, ok = b.Dequeue()
-	assert.True(t, ok)
-	assert.Equal(t, 2, *v)
-	v, ok = b.Dequeue()
-	assert.True(t, ok)
-	assert.Equal(t, 3, *v)
-	v, ok = b.Dequeue()
-	assert.True(t, ok)
-	assert.Equal(t, 5, *v)
-
-	assert.Panics(t, func() {
-		_ = New[int](3)
+	t.Run("empty queue must return false", func(t *testing.T) {
+		v, ok := b.Dequeue()
+		assert.False(t, ok)
+		assert.Nil(t, v)
 	})
 
-	assert.Panics(t, func() {
-		_ = New[int](0)
+	t.Run("enqueue one and dequeue one", func(t *testing.T) {
+		ok := b.Enqueue(intPtr(0))
+		assert.True(t, ok)
+
+		v, ok := b.Dequeue()
+		assert.True(t, ok)
+		assert.Equal(t, 0, *v)
+	})
+
+	t.Run("enqueue until buffer is full", func(t *testing.T) {
+		ok := b.Enqueue(intPtr(1))
+		assert.True(t, ok)
+		ok = b.Enqueue(intPtr(2))
+		assert.True(t, ok)
+		ok = b.Enqueue(intPtr(3))
+		assert.True(t, ok)
+		ok = b.Enqueue(intPtr(4))
+		assert.False(t, ok)
+	})
+
+	t.Run("dequeue one and check value", func(t *testing.T) {
+		v, ok := b.Dequeue()
+		assert.True(t, ok)
+		assert.Equal(t, 1, *v)
+	})
+
+	t.Run("enqueing one after dequeueing one must succeed", func(t *testing.T) {
+		ok := b.Enqueue(intPtr(5))
+		assert.True(t, ok)
+	})
+
+	t.Run("dequeue until buffer is empty", func(t *testing.T) {
+		v, ok := b.Dequeue()
+		assert.True(t, ok)
+		assert.Equal(t, 2, *v)
+		v, ok = b.Dequeue()
+		assert.True(t, ok)
+		assert.Equal(t, 3, *v)
+		v, ok = b.Dequeue()
+		assert.True(t, ok)
+		assert.Equal(t, 5, *v)
+
+		v, ok = b.Dequeue()
+		assert.False(t, ok)
+		assert.Nil(t, v)
+	})
+
+	t.Run("creating queues with invalid sizes must panic", func(t *testing.T) {
+		assert.Panics(t, func() {
+			_ = New[int](3)
+		})
+
+		assert.Panics(t, func() {
+			_ = New[int](0)
+		})
 	})
 }
 
